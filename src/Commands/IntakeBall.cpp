18538ddd9a1f89beef3dc6638dccbf4001dd3f83
dhrutip <dhruti.pandya@gmail.com>
2016-09-29 11:52:58 -0700

    first commit

---
+++ src/Commands/IntakeBall.cpp
@@ -0,0 +1,38 @@
+#include "IntakeBall.h"
+
+IntakeBall::IntakeBall()
+{
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(chassis);
+}
+
+// Called just before this Command runs the first time
+void IntakeBall::Initialize()
+{
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void IntakeBall::Execute()
+{
+
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool IntakeBall::IsFinished()
+{
+	return false;
+}
+
+// Called once after isFinished returns true
+void IntakeBall::End()
+{
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void IntakeBall::Interrupted()
+{
+
+}
