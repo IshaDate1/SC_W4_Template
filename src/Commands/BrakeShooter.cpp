c57cbe8d65a1715dfe5677dc09bb677a6c0cd31f
IshaDate1 <tekgeek9@gmail.com>
2016-09-30 18:01:04 -0700

    i.Shooter

---
+++ src/Commands/BrakeShooter.cpp
@@ -0,0 +1,38 @@
+#include "BrakeShooter.h"
+
+BrakeShooter::BrakeShooter()
+{
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(chassis);
+}
+
+// Called just before this Command runs the first time
+void BrakeShooter::Initialize()
+{
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void BrakeShooter::Execute()
+{
+
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool BrakeShooter::IsFinished()
+{
+	return false;
+}
+
+// Called once after isFinished returns true
+void BrakeShooter::End()
+{
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void BrakeShooter::Interrupted()
+{
+
+}
--- src/Subsystems/Shooter.cpp
+++ src/Subsystems/Shooter.cpp
@@ -2,7 +2,7 @@
 #include "../RobotMap.h"
 
 Shooter::Shooter() :
-		Subsystem("ExampleSubsystem")
+		Subsystem("Shooter")ShooterMotor (new Talon(4));
 {
 
 }
@@ -11,7 +11,19 @@ void Shooter::InitDefaultCommand()
 {
 	// Set the default command for a subsystem here.
 	//SetDefaultCommand(new MySpecialCommand());
+	SetDefaultCommand (new ShootBall(), new BrakeShooter());
 }
+void Shooter::ShootBall()
+{
+	ShooterMotor->Set(1);
+}
+void Shooter::BrakeShooter()
+{
+	ShooterMotor->Set(0);
+}
+
+
+
 
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
